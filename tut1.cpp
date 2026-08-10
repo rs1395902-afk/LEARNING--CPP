56993508987bccf683f6a9bf0ad63d426cb29f68
MOHIT-PYTHON <rs1395902@gmail.com>
2026-08-10 19:48:28 +0530

    DAY 01 OF LEARNING C++

---
+++ tut1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"HELLO BHIRU";
+    cout<<"\nI HAVE ALREADY LEARNED PYTHON";
+    cout<<"\nNOW I AM LEARNNING C++";
+    cout<<"\nI WANT TO BECOME A SOFTWRE ENGINEER";
+    cout<<"\nI WILL START DSA AFTER LEARNING C++";
+    cout<<"\nMY DREAM COMANY IS GOOGLE";
+    cout<<"\nTHANK YOU";
+
+    return 0 ;
+    
+}
---
+++ tut2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // int a = 1;
+    // int b = 19;
+    int a=1,b=2;
+    float c = 1.2;
+    double d = 1.32;
+    char ch = 'A';
+    bool is_true = true;
+    
+cout<<"HELLO WORD, I AM LEARNING C++ FOR "<< a << " YEARS AND " << b << " MONTHS";
+cout<<"\nTHE VALUE OF C IS: "<< c;
+cout<<"\nTHE VALUE OF D IS: "<< d;
+cout<<"\nTHE VALUE OF CH IS: "<< ch;
+cout<<"\nTHE VALUE OF IS_TRUE IS: "<< is_true; // "OUTPUT MAY BE 1 OR 0, 1 FOR TRUE AND 0 FOR FALSE"
+return 0;
+}
