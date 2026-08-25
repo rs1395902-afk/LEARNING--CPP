4c6cc1ecccf4704c6fecd71b192caa4163859860
MOHIT-PYTHON <rs1395902@gmail.com>
2026-08-25 20:39:57 +0530

    DAY 6 OF LEARNING C++

---
+++ tut6.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //FOR LOOP ==>(INTIALIZATION;CONDITION;UPDATION)
+    // for(int i=0; i<=100; i++){
+    //     cout<<"I AM GOOD BOY"<<endl;
+    // }
+
+    //WHILE LOOP ==>(CONDITION)
+    // int i=1;
+    // while(i<=10){
+    // cout<<"FUTRE PROGRAMMER"<<endl;
+    // i++;
+    // }
+
+    //DO WHILE : (CONDITION AGR FALSE BHI RAHEGA TO CODE 1 BAAR EXECUTE HOGA)
+    int i=1;
+    cout<<"TABLE OF 6"<<endl;
+    do{
+        cout<<"6 x 1 ="<<(6*i)<<endl;
+        i++;
+    }while(i<=10);
+
+    return 0;
+
+}
